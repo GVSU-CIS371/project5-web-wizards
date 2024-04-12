2f9f0e0be94ea8ca6f0750fd10edaf0836073115
Alex <alexe0336@gmail.com>
2024-04-12 16:30:21 -0400

    Add category filtering to Clothing.vue, BestSeller.vue, Groceries.vue, and Electronics.vue components

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -1,28 +1,37 @@
 <template>
-  <v-card v-for="product in products" :key="product.id">
+  <v-card>
     <v-img :src="product.data.image" height="200px"></v-img>
     <v-card-title>{{ product.data.name }}</v-card-title>
     <v-card-subtitle>{{ product.data.category }}</v-card-subtitle>
     <v-card-text>{{ product.data.description }}</v-card-text>
-    <v-card-actions style="display: flexbox;">
-      <v-btn>Price: ${{ product.data.price }}</v-btn>
-      <v-btn>Rating: {{ product.data.rating }}</v-btn>
-      <v-btn>Stock: {{ product.data.stock }}</v-btn>
-    </v-card-actions>
+    <v-card-actions style="display: flex;">
+  <v-btn>
+    <v-icon color="green" left>mdi-cash</v-icon>
+    Price: ${{ product.data.price }}
+  </v-btn>
+  <v-btn>
+    <v-icon color="yellow" left>mdi-star-outline</v-icon>
+    Rating: {{ product.data.rating }}
+  </v-btn>
+  <v-btn>
+    <v-icon color="blue" left>mdi-truck-check-outline</v-icon>
+    Stock: {{ product.data.stock }}
+  </v-btn>
+</v-card-actions>
+
   </v-card>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { useProductStore } from '../stores/ProductStore';
+import { defineComponent, PropType } from 'vue';
+import { ProductDoc } from '../types/product'; // Import the type definition
 
 export default defineComponent({
-  setup() {
-    const productStore = useProductStore();
-
-    return {
-      products: productStore.products,
-    };
-  },
+  props: {
+    product: {
+      type: Object as PropType<ProductDoc>,
+      required: true
+    }
+  }
 });
 </script>
